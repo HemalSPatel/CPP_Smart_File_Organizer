b4f48b1d0d153fbc5e235d4356aced4303a2efc3
Hemal Patel <95436169+HemalSPatel@users.noreply.github.com>
2025-12-12 23:48:38 -0500

    error handling

--- main.cpp
+++ main.cpp
@@ -6,14 +6,29 @@ namespace fs = std::filesystem;
 int main() {
     //fs::path start_path = "/Users/hemal/Desktop/Niyu Pics";
     fs::path start_path = "/Users/hemal/Desktop/Neetcode";
-
+    std::error_code ec;
     std::cout << "Displaying contents of: " << (start_path) << std::endl;
 
     try {
-        for (const auto& entry : fs::recursive_directory_iterator(start_path)) {
+        if (!fs::exists(start_path))
+        {
+            std::cerr << "Path " << start_path << " does not exist." << std::endl;
+            return 1;
+        }
+        for (const auto& entry : fs::recursive_directory_iterator(start_path, ec)) {
+            if (ec)
+            {
+                std::cerr << "Error accessing: " << entry.path().string() << " - " << ec.message() << std::endl;
+                ec.clear();
+                continue;
+            }
             const auto& path = entry.path();
-            std::cout << (is_directory(path) ? "[DIR]" : "[FILE]") << " ";
-            std::cout << path.parent_path().filename() << " - " << path.filename() <<  std::endl;
+
+            std::cout << (fs::is_directory(path) ? "[DIR]" : "[FILE]") << " ";
+            std::cout << path.parent_path().filename() << " - " << path.stem() << path.extension() <<  std::endl;
+        }
+        if (ec) {
+            std::cerr << "An error occurred during final iteration: " << ec.message() << std::endl;
         }
     } catch (const fs::filesystem_error& e) {
         std::cerr << "Filesystem error: " << e.what() << std::endl;
